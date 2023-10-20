991bee9d1b6caf1919b085300b166c4ca301a662
rochelleaguila <143100232+rochelleaguila@users.noreply.github.com>
2023-10-20 02:11:12 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
 };
+let pronouns = ["my", "her"];
+let adjectives = ["terrific", "wonderful", "amazing", "pretty"];
+let nouns = ["jogger", "musichall", "castle", "hotel"];
+let domains = [];
+
+const generateDomains = () => {
+  for (let i = 0; i < pronouns.length; i++) {
+    for (let j = 0; j < adjectives.length; j++) {
+      for (let k = 0; k < nouns.length; k++) {
+        let domain = pronouns[i] + adjectives[j] + nouns[k] + ".com";
+        domains.push(domain);
+      }
+    }
+  }
+  displayDomains();
+};
+
+const displayDomains = () => {
+  const domainContainer = document.getElementById("domainContainer");
+  domainContainer.innerHTML = "";
+
+  domains.forEach(domain => {
+    const domainElement = document.createElement("p");
+    domainElement.textContent = domain;
+    domainContainer.appendChild(domainElement);
+  });
+};
+
+const generateButton = document.getElementById("generateButton");
+generateButton.addEventListener("click", generateDomains);
